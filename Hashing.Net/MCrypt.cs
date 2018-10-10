7d1c8bc4dc769ce3769c27e276e51959998e6072
Anders Kalhauge <anders@kalhauge.dk>
2018-10-10 09:55:59 +0200

    Execptions in C# example

--- Hashing.Net/MCrypt.cs
+++ Hashing.Net/MCrypt.cs
@@ -1,3 +1,10 @@
+using System;
+
 public class MCrypt {
-    public static string HashPassword(string password, string salt)
+    public static string HashPassword(string password, string salt) {
+        throw new NotImplementedException();
+        }
+    public static bool Verify(string candidate, string hash) {
+        throw new NotImplementedException();
+        }   
     }
